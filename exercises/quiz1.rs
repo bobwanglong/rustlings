e44c4b7a0d015ef740615f594204caa766e14bc5
class-bob <wanglongtin@163.com>
2023-05-26 10:39:14 +0800

    fix:variable if func quiz1 done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (num:i32)->i32{
+    let mut applePrice = 2;
+    if num >40{
+        applePrice=1;
+        return applePrice*num
+    }else{
+        return applePrice*num
+    }
+}
 
 // Don't modify this function!
 #[test]
