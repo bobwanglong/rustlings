440cf9bdc4fbb3919ad526ce0f1be68be64eabaf
class-bob <wanglongtin@163.com>
2023-06-03 19:19:53 +0800

    fix: string exercises done

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -6,8 +6,6 @@
 // before the parentheses on each line. If you're right, it will compile!
 // No hints this time!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -16,14 +14,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue"); // &str
+    string("red".to_string()); // String
+    string(String::from("hi")); // String
+    string("rust is fun!".to_owned()); // String
+    string("nice weather".into()); //? &str->
+    string(format!("Interpolation {}", "Station")); // String
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim()); // &str
+    string("Happy Monday!".to_string().replace("Mon", "Tues")); // String
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase()); //?
 }
